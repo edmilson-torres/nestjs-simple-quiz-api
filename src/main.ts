99bf98d5c91da4e85f213779f7a7bc7c9b0005cb
Edmilson Torres <edmilson.torres@gmail.com>
2024-04-27 17:27:02 -0300

    feat: config PORT

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+    const configService = app.get(ConfigService);
 
-  const config = new DocumentBuilder()
-    .setTitle('Simple Quiz')
-    .setDescription('Quiz')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+    app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+    const config = new DocumentBuilder()
+        .setTitle('Simple Quiz')
+        .setDescription('Quiz')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    await app.listen(configService.get<number>('PORT', { infer: true }));
 }
+
 bootstrap();
