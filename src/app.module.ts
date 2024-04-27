6ec7058e0c2191ddf0ad2029fd7cd7f94c631988
Edmilson Torres <edmilson.torres@gmail.com>
2024-04-27 18:11:44 -0300

    feat: config global throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
-        })
+        }),
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60000,
+                limit: 100
+            }
+        ])
     ],
     controllers: [],
     providers: []
