b2bb77c700441b7a7dea8082377ef80a2ad65e62
Edmilson Torres <edmilson.torres@gmail.com>
2024-04-27 18:43:18 -0300

    feat: config multiple throttler definitions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
     imports: [
@@ -9,12 +10,28 @@ import { ThrottlerModule } from '@nestjs/throttler';
         }),
         ThrottlerModule.forRoot([
             {
+                name: 'short',
+                ttl: 1000,
+                limit: 3
+            },
+            {
+                name: 'medium',
+                ttl: 10000,
+                limit: 20
+            },
+            {
+                name: 'long',
                 ttl: 60000,
                 limit: 100
             }
         ])
     ],
     controllers: [],
-    providers: []
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        }
+    ]
 })
 export class AppModule {}
