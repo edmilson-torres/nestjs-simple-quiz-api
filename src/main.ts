bffb8e0b2bcddfd72376b568d0189ee80786e0e1
Edmilson Torres <edmilson.torres@gmail.com>
2024-05-05 10:04:32 -0300

    style: remove semi

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,29 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import helmet from 'helmet';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './http-exception.filter';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import helmet from 'helmet'
+import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './http-exception.filter'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule)
 
-    app.useGlobalFilters(new HttpExceptionFilter(new Logger('HTTP')));
-    app.enableCors();
-    app.use(helmet());
-    const configService = app.get(ConfigService);
-
-    app.useGlobalPipes(new ValidationPipe());
+    const configService = app.get(ConfigService)
+    app.useGlobalFilters(new HttpExceptionFilter(new Logger('HTTP')))
+    app.enableCors()
+    app.use(helmet())
+    app.useGlobalPipes(new ValidationPipe())
 
     const config = new DocumentBuilder()
         .setTitle('Simple Quiz')
         .setDescription('Quiz')
         .setVersion('1.0')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('docs', app, document)
 
-    await app.listen(configService.get<number>('PORT', { infer: true }));
+    await app.listen(configService.get<number>('PORT', { infer: true }))
 }
 
-bootstrap();
+bootstrap()
