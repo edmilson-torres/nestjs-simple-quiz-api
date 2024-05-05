1bb30089a85fbae3973132c7a054c1e7bc5efedf
Edmilson Torres <edmilson.torres@gmail.com>
2024-05-05 10:04:07 -0300

    refactor: better message

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -4,33 +4,27 @@ import {
     ExceptionFilter,
     HttpException,
     Logger
-} from '@nestjs/common';
-import { hrtime } from 'process';
+} from '@nestjs/common'
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     constructor(private logger: Logger) {}
 
     catch(exception: HttpException, host: ArgumentsHost): void {
-        const startAt = hrtime();
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse()
+        const request = ctx.getRequest()
+        const status = exception.getStatus()
 
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
-        const request = ctx.getRequest();
-        const status = exception.getStatus();
+        const message = exception.message
 
-        const message = exception.message;
-
-        const { method, originalUrl } = request;
+        const { method, originalUrl } = request
 
         response.on('finish', () => {
-            const diff = hrtime(startAt);
-            const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
-
             this.logger.error(
-                `\x1B[32m ${method} ${originalUrl} \x1B[37m${status} \x1B[31m${JSON.stringify(message)} \x1B[33m${responseTime.toFixed(2)}ms`
-            );
-        });
+                `\x1B[32m ${method} ${originalUrl} \x1B[37m${status} \x1B[31m${message}`
+            )
+        })
 
         response.status(status).json({
             statusCode: status,
@@ -38,6 +32,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
             method,
             path: originalUrl,
             timestamp: new Date().toISOString()
-        });
+        })
     }
 }
