3b4677c53200422ff2a6f806df2b08506c26154d
Edmilson Torres <edmilson.torres@gmail.com>
2024-05-12 17:01:54 -0300

    refactor: remove global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
-import { Logger, ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import helmet from 'helmet'
 import { AppModule } from './app.module'
-import { HttpExceptionFilter } from './http-exception.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     const configService = app.get(ConfigService)
-    app.useGlobalFilters(new HttpExceptionFilter(new Logger('HTTP')))
     app.enableCors()
     app.use(helmet())
     app.useGlobalPipes(new ValidationPipe())
