1619630012c1451741816648289c2b6f6a843b0f
Edmilson Torres <edmilson.torres@gmail.com>
2024-06-08 11:23:25 -0300

    fix: change src path to absolute

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
 import { jwtConstants } from '../auth.config'
 import { JwtTokenPayload } from '../auth.service'
-import { UserEntity } from 'src/users/entities/user.entity'
+import { UserEntity } from '../../users/entities/user.entity'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
