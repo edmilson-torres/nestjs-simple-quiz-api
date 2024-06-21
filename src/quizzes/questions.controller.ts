3c4f01f7b3138f1cc12a08d2d0914a56b11b87b4
Edmilson Torres <edmilson.torres@gmail.com>
2024-06-21 08:26:52 -0300

    feat: questions crud

---
+++ src/quizzes/questions.controller.ts
@@ -0,0 +1,65 @@
+import {
+    UseGuards,
+    UseInterceptors,
+    ClassSerializerInterceptor,
+    Controller,
+    Body,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    ParseUUIDPipe,
+    Patch,
+    Post
+} from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger'
+
+import { Roles } from '../auth/decorators/roles.decorator'
+import { RolesEnum } from '../users/entities/user.entity'
+import { RolesGuard } from '../auth/guards/roles.guard'
+import { QuestionsService } from './questions.service'
+import { QuestionDto } from './dto/question.dto'
+
+@ApiTags('Questions')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@UseInterceptors(ClassSerializerInterceptor)
+@Controller('questions')
+export class QuestionsController {
+    constructor(private readonly questionService: QuestionsService) {}
+
+    @Roles(RolesEnum.Admin, RolesEnum.Moderator)
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() payload: QuestionDto) {
+        return this.questionService.create(payload)
+    }
+
+    @Get()
+    findAll() {
+        return this.questionService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseUUIDPipe) id: string) {
+        return this.questionService.findOne(id)
+    }
+
+    @Roles(RolesEnum.Admin, RolesEnum.Moderator)
+    @Patch(':id')
+    update(
+        @Param('id', ParseUUIDPipe) id: string,
+        @Body() payload: QuestionDto
+    ) {
+        return this.questionService.update(id, payload)
+    }
+
+    @Roles(RolesEnum.Admin, RolesEnum.Moderator)
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id', ParseUUIDPipe) id: string) {
+        return this.questionService.remove(id)
+    }
+}
---
+++ src/quizzes/questions.service.ts
@@ -0,0 +1,77 @@
+import {
+    BadRequestException,
+    ConflictException,
+    Injectable,
+    NotFoundException
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository, TypeORMError } from 'typeorm'
+
+import { QuestionEntity } from './entities/question.entity'
+import { QuestionDto } from './dto/question.dto'
+
+@Injectable()
+export class QuestionsService {
+    @InjectRepository(QuestionEntity)
+    private readonly questionsRepository: Repository<QuestionEntity>
+
+    async create(payload: QuestionDto) {
+        const question = this.questionsRepository.create(payload)
+
+        try {
+            const newQuestion = await this.questionsRepository.save(question)
+
+            return new QuestionEntity(newQuestion)
+        } catch (error) {
+            throw new BadRequestException(error.message)
+        }
+    }
+
+    async findOne(id: string) {
+        const foundedQuestion = await this.questionsRepository.findOne({
+            where: { id }
+        })
+
+        if (!foundedQuestion) {
+            throw new NotFoundException()
+        }
+
+        return new QuestionEntity(foundedQuestion)
+    }
+
+    findAll() {
+        return this.questionsRepository.find()
+    }
+
+    async update(id: string, payload: QuestionDto) {
+        const questionChecked = await this.questionsRepository.exists({
+            where: { id }
+        })
+
+        if (!questionChecked) {
+            throw new NotFoundException()
+        }
+
+        const question = this.questionsRepository.create(payload)
+
+        return this.questionsRepository.update({ id }, question)
+    }
+
+    async remove(id: string) {
+        const questionChecked = await this.questionsRepository.exists({
+            where: { id }
+        })
+
+        if (!questionChecked) {
+            throw new NotFoundException()
+        }
+
+        try {
+            await this.questionsRepository.delete(id)
+        } catch (error) {
+            if (error instanceof TypeORMError) throw new ConflictException()
+        }
+
+        return null
+    }
+}
