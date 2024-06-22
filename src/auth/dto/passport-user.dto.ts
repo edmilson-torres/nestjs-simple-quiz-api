fa65e921afa380d347e640d0fbb73f4ae944665c
Edmilson Torres <edmilson.torres@gmail.com>
2024-06-22 08:57:18 -0300

    chore: use relative path

--- src/auth/dto/passport-user.dto.ts
+++ src/auth/dto/passport-user.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsEnum, IsUUID } from 'class-validator'
-import { RolesEnum } from 'src/users/entities/user.entity'
+import { RolesEnum } from '../../users/entities/user.entity'
 
 export class PassportUserDto {
     @IsUUID()
