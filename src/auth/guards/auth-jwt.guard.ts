50bac48b976b38674c67fd2507c3e9ab182291db
Edmilson Torres <edmilson.torres@gmail.com>
2024-06-22 09:28:32 -0300

    fix: auth guard jwt using passport

--- src/auth/guards/auth-jwt.guard.ts
+++ src/auth/guards/auth-jwt.guard.ts
@@ -1,51 +1,25 @@
-import {
-    Injectable,
-    CanActivate,
-    ExecutionContext,
-    UnauthorizedException
-} from '@nestjs/common'
-import { Reflector } from '@nestjs/core'
-import { JwtService } from '@nestjs/jwt'
+import { Injectable, ExecutionContext } from '@nestjs/common'
 
-import { jwtConstants } from '../auth.config'
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator'
-import { Request } from 'express'
+import { AuthGuard } from '@nestjs/passport'
+import { Reflector } from '@nestjs/core'
 
 @Injectable()
-export class AuthGuardJwt implements CanActivate {
-    constructor(
-        private jwtService: JwtService,
-        private reflector: Reflector
-    ) {}
+export class AuthGuardJwt extends AuthGuard('jwt') {
+    constructor(private reflector: Reflector) {
+        super()
+    }
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
+    canActivate(context: ExecutionContext) {
         const isPublic = this.reflector.getAllAndOverride<boolean>(
             IS_PUBLIC_KEY,
             [context.getHandler(), context.getClass()]
         )
+
         if (isPublic) {
             return true
         }
 
-        const request = context.switchToHttp().getRequest()
-        const token = this.extractTokenFromHeader(request)
-        if (!token) {
-            throw new UnauthorizedException()
-        }
-        try {
-            const payload = await this.jwtService.verifyAsync(token, {
-                secret: jwtConstants.secret
-            })
-
-            request['user'] = payload
-        } catch {
-            throw new UnauthorizedException()
-        }
-        return true
-    }
-
-    private extractTokenFromHeader(request: Request): string | undefined {
-        const [type, token] = request.headers.authorization?.split(' ') ?? []
-        return type === 'Bearer' ? token : undefined
+        return super.canActivate(context)
     }
 }
