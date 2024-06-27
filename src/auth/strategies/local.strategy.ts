05340817bc85f72fd9eafc21a71910ff2cdaabb9
Edmilson Torres <edmilson.torres@gmail.com>
2024-06-27 14:15:15 -0300

    feat: validate login dto

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,7 +1,13 @@
 import { Strategy } from 'passport-local'
 import { PassportStrategy } from '@nestjs/passport'
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import {
+    BadRequestException,
+    Injectable,
+    UnauthorizedException
+} from '@nestjs/common'
 import { AuthService } from '../auth.service'
+import { validate } from 'class-validator'
+import { LoginDto } from '../dto/login.dto'
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,6 +16,23 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(email: string, password: string): Promise<any> {
+        const loginDto = new LoginDto()
+        loginDto.email = email
+        loginDto.password = password
+
+        const errors = await validate(loginDto)
+        if (errors.length > 0) {
+            const constraints = []
+
+            for (const error of errors) {
+                constraints.push(
+                    error.constraints ? Object.values(error.constraints) : []
+                )
+            }
+
+            throw new BadRequestException(constraints.toString())
+        }
+
         const user = await this.authService.validateUser(email, password)
         if (!user) {
             throw new UnauthorizedException()
