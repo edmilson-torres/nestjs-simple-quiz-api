2651813d278964c7598a8eaa81791fd151bcfefb
Edmilson Torres <edmilson.torres@gmail.com>
2024-07-05 23:15:37 -0300

    fix(user): is admin function

--- src/users/helpers/isAdmin.ts
+++ src/users/helpers/isAdmin.ts
@@ -1,7 +1,5 @@
 import { RoleEnum } from '../entities/role.enum'
 
 export const isAdmin = (role: RoleEnum) => {
-    if (!Array.isArray(role) || role.length === 0) return false
-
-    return role.some((role) => role === RoleEnum.Admin)
+    return role === RoleEnum.Admin
 }
