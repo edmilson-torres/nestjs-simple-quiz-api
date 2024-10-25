6cdbe963ce01ad62214eaf66f9d7b6f6209559e5
Edmilson Torres <edmilson.torres@gmail.com>
2024-10-25 10:26:50 -0300

    feat: expose swagger json

--- src/main.ts
+++ src/main.ts
@@ -27,9 +27,14 @@ async function bootstrap() {
         .addBearerAuth()
         .build()
     const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('docs', app, document)
+    SwaggerModule.setup('docs', app, document, {
+        jsonDocumentUrl: 'docs-json'
+    })
 
-    await app.listen(configService.get<number>('PORT', { infer: true }) || 3000)
+    await app.listen(
+        configService.get<number>('PORT', { infer: true }) || 3000,
+        '0.0.0.0'
+    )
 
     console.log(`Application is running on: ${await app.getUrl()}`)
 }
